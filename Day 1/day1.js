908fbefc8e66bb1b3feb8111c85c194ed8b40ef9
hamzashakoor119 <135787611+hamzashakoor119@users.noreply.github.com>
2024-03-05 13:08:13 -0700

    Add files via upload

---
+++ Day 1/day1.js
@@ -0,0 +1,20 @@
+// Q-1 = install Node Js , TypeScript and VS Code on your Laptop .
+// Answer = All Installatioin Dn .
+// __________________________________________________________________
+// Q-2 = Personal Message: Store a person’s name in a variable and print a message to them, like 
+//" Hello Hamza, How Was Your First Day 100 Days Of Coding " ?
+// __________________________________________________________________________________________________
+// Code is here :
+var person_name1 = " Hamza ";
+console.log("Hello ".concat(person_name1, " How Was Your First Day 100 Days Of Coding ? "));
+// Q-3 = Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// _________________________________________________________________________________________________________________________________
+// Code is here :
+var person_name2 = " Hamza Shakoor ";
+// (i)- Show Person Name in LowerCase :
+var lowercaseName = person_name2.toLowerCase();
+console.log(lowercaseName);
+// (ii)- Show Person Name in UpperCase :
+var uppercaseName = person_name2.toUpperCase();
+console.log(uppercaseName);
+// (iii)- Show Person Name in TitleCase :
---
+++ Day 1/day1.ts
@@ -0,0 +1,36 @@
+
+// Q-1 = install Node Js , TypeScript and VS Code on your Laptop .
+// Answer = All Installatioin Dn .
+// __________________________________________________________________
+
+// Q-2 = Personal Message: Store a person’s name in a variable and print a message to them, like 
+//" Hello Hamza, How Was Your First Day 100 Days Of Coding " ?
+// __________________________________________________________________________________________________
+
+// Code is here :
+
+
+let person_name1:string = " Hamza ";
+console.log(`Hello ${person_name1} How Was Your First Day 100 Days Of Coding ? `);
+
+
+
+// Q-3 = Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// _________________________________________________________________________________________________________________________________
+
+// Code is here :
+
+
+let person_name2: string = " Hamza Shakoor ";
+
+// (i)- Show Person Name in LowerCase :
+
+let lowercaseName: string = person_name2.toLowerCase();
+console.log(lowercaseName);
+
+// (ii)- Show Person Name in UpperCase :
+
+let uppercaseName: string = person_name2.toUpperCase(); 
+console.log(uppercaseName);
+
+// (iii)- Show Person Name in TitleCase :
