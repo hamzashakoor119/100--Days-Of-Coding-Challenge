a775599c0123e507899d16f6074238a0c6b7e25b
hamzashakoor119 <135787611+hamzashakoor119@users.noreply.github.com>
2024-03-06 15:17:45 -0700

    Add files via upload

---
+++ Day 3/day3.js
@@ -0,0 +1,35 @@
+// ============================================================================ Day-3 Assignments ============================================================================
+// Q-7 = Number Eight: Write addition, subtraction, multiplication,and division
+// operations that each result in the number 8. Enclose your operations in print statements ?
+// _________________________________________________________________________________________________
+// Code is here :
+// Addition :
+console.log(5 + 3);
+// Subtractions :
+console.log(10 - 2);
+// Multiplication :
+console.log(2 * 4);
+// Division :
+console.log(16 / 2);
+// Q-8 = You should create four lines that look like this:
+// `console.log(5 + 3)`
+// Your output should simply be four lines with the number 8 appearing once on each line. ?
+// ________________________________________________________________________________________________________________
+// Code is here :
+console.log(8);
+console.log(8);
+console.log(8);
+console.log(8);
+// Q-9 = Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number.
+// Print that message ?
+// ______________________________________________________________________________________________________________________________________
+// Code is here :
+// N-1 :
+var favourite_num1 = 1;
+console.log(" Yeeeh ' ".concat(favourite_num1, " ' ,is my lucky number ."));
+// N-2 :
+var favourite_num2 = 8;
+console.log(" Oh ' ".concat(favourite_num2, " ' ,is my favourite number ."));
+// N-3 :
+var favourite_num3 = 11;
+console.log(" My favourite No is = ".concat(favourite_num3, " ."));
